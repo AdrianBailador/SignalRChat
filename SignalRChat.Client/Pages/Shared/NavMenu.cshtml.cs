dde56801f22607df154f16353cf005d7ad360bc6
Adrián Bailador Panero <adrianbailador@hotmail.com>
2022-08-25 23:21:07 +0100

    Pages

---
+++ SignalRChat.Client/Pages/Shared/NavMenu.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SignalRChat.Client.Pages.Shared
+{
+    public class NavMenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
