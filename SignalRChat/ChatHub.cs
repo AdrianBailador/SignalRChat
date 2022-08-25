a67e3c939a5cb192fdacfe64c2928c06abaf00e7
Adrián Bailador Panero <adrianbailador@hotmail.com>
2022-08-25 22:44:42 +0100

    Agregar archivos de proyecto.

---
+++ SignalRChat/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRChat.Server
+{
+    class ChatHub : Hub
+    {
+        //Metodo que va a enviar y recibir mensajes
+        public async Task SendMessage(string user, string message)
+        {
+            await Client.All.SendAsync("Receivessage", user, message);
+        }
+    }
+}
